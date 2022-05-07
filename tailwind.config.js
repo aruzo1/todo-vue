23152c570e5001b4516c2e9190a05983448742f9
Aruzo <dawid12391111@gmail.com>
2022-05-08 00:07:06 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.vue"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#4f46e5",
+          light: "#6366f1",
+          dark: "#4338ca",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
